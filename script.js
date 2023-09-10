1db3a13a97d1f55a1cfca48d667fc72321fe35bd
brajamohamed <136047508+brajamohamed@users.noreply.github.com>
2023-09-10 10:44:11 +0530

    added portfolio

---
+++ script.js
@@ -0,0 +1,9 @@
+let name = document.getElementById("name");
+let company = document.getElementById("company");
+let email = document.getElementById("email");
+
+let submit = document.getElementById("submit");
+submit.addEventListener("click", () => checkform());
+
+function checkform() {}
+alert("Submitted Successfully..");
